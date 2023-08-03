1ccb02d970b144a3fd0c0347d89f812a60e8da6e
Evening <93998243+Eveningg@users.noreply.github.com>
2023-08-03 20:22:01 +0100

    Time: 3451 ms (14.85%), Space: 14 MB (91.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,11 +6,15 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        for i, j in enumerate(nums):
-
-            for o, p in enumerate(nums):
-
-                if p + j == target and i != o:
-                    return(i, o)
+        # Iterating Through Integer List
+        for counter, integer in enumerate(nums):
+            
+            # Iterating Through Integer List Again
+            for counter2, integer2 in enumerate(nums):
+                
+                if integer + integer2 == target and counter != counter2:
+                    
+                    return(counter,counter2)
+            
 
     
