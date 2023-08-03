0eaa8771f2a1d761397ba9a3a2d044b4934d8f40
Evening <93998243+Eveningg@users.noreply.github.com>
2023-08-03 21:38:51 +0100

    Time: 30 ms (99.87%), Space: 16.2 MB (82.78%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,10 +6,17 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
+        # Overall algorithm compares each element in nodes sequentially, placing smallest value in dummyNode
+        
+        # dummyNode = temp node to act as temp storage
+        # Tail - equals the value of dummyNode
         dummyNode = ListNode()
         tail = dummyNode
         
+        # While loops iterates until one list is empty
         while list1 and list2:
+            
+            # Placing the smallest value of both lists into dummyNode, then moving to next element of corresponding node
             if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
@@ -18,8 +25,10 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             
+            #moving onto next node position after executing above conditionals
             tail = tail.next
         
+        # Add either list1 or list2 to the end of our dummyNode, as the lists are already sorted
         if list1:
             tail.next = list1
             
