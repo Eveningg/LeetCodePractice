5dc0869d3bd62111e51a153fea1b17757a7f27c6
Evening <93998243+Eveningg@users.noreply.github.com>
2023-08-03 01:24:15 +0100

    Time: 42 ms (89.04%), Space: 16.3 MB (70.39%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        
+        result = ""
+        
+        # Iterating Code The Number Of Times Equal To First String Length of list "str"
+        for i in range(len(strs[0])):
+
+            # Iterating Through Every String In List "Str"
+            for string in strs:
+                
+                # If Character I Not Equal To Characters At Same Index Of Other Strings, Return Result
+                # If 'I' is equal to length of string being read, then program ends
+                # If First Condition Is True, Other Result Not Read
+                if i == len(string) or string[i] != strs[0][i]:
+                    return result
+            
+            result += strs[0][i]
+            
+        return result
+                
+
+        
