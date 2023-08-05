63622761b6e01ff3c53d0f8c8c3c194d7a2637da
Evening <93998243+Eveningg@users.noreply.github.com>
2023-08-05 01:25:21 +0100

    Time: 510 ms (90.72%), Space: 28.5 MB (93.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,4 +1,23 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        return len(set(nums))!=len(nums)
+        # Pointer Solution
+        
+        pointer = 1
+        
+        nums.sort()
+        
+        for i in nums:
+            
+            if pointer >= len(nums):
+                return False
+            
+            elif i == nums[pointer]:
+                return True
+            
+            pointer += 1
+                
+    
+        # This solution works.
+        # If the original array isn't the same size as a set of the array, then the original array had duplicate elements.
+        # return len(set(nums))!=len(nums)
