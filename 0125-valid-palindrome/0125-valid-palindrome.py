7f9e2a6f58cf57acec0a3fdec6e7f658f76f2017
Evening <93998243+Eveningg@users.noreply.github.com>
2023-08-19 01:32:24 +0100

    Time: 56 ms (65.01%), Space: 17.1 MB (82.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
+        # Solution Using In-Built Functions.
+        
         newStr = ""
         
         for char in s:
@@ -9,3 +11,10 @@ class Solution:
                 newStr += char.lower()
                 
         return newStr == newStr[::-1]
+    
+        # Big O(n) Solution.
+        
+        def alphaNum(self,c):
+            return (ord('A') <= ord(c) <= ord('Z') or
+                    ord('a') <= ord(c) <= ord('z') or
+                    ord('0') <= ord(c) <= ord('9'))
