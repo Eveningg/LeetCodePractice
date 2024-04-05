30856c34030638256d3ea3c12a7ebb3a4d2df5d8
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-05 09:42:40 +0100

    Time: 51 ms (67.49%), Space: 16.5 MB (95.15%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.py
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -1,27 +1,14 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        # arr nums - even length
-        # empty arr arr
         
-        # alice will remove the minimum element from nums
-        # bob does the same
+        nums.sort()
+        i=0
         
-        # alice will append the removed elemeent in the array arr
-        # alice does the same
-        
-        arr = []
-        
-        while len(nums) > 0:
+        while(i<len(nums)):
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+            i+=2
             
-            removedNum = min(nums)
-            nums.remove(min(nums))
-            removedNum2 = min(nums)
-            nums.remove(min(nums))
-                
-            arr.append(removedNum2)
-            arr.append(removedNum)
-                
-        return arr
+        return nums     
                 
             
                 
