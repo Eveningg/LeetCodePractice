b9a2ebbd5abf0a1b52c0f352d576c66cd1e5fda1
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-08 11:57:03 +0100

    Time: 34 ms (62.91%), Space: 16.4 MB (86.15%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,6 +1,5 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        #command that consistents of strings: "G", "()", "(al)"
         
         newString = command.replace("()", "o")
         newString = newString.replace("(al)", "al")
