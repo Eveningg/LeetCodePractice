d6b6e3a5ce1eedd517b2b17d7df180baf1d4f622
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-09 13:00:33 +0100

    Time: 71 ms (63.12%), Space: 16.8 MB (19.18%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.py
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.py
@@ -5,9 +5,14 @@ class Solution:
             return nums[0]
         
         d, j, res = [0] * len(nums), 1, 0
+        
         for i in range(1, len(nums)):
-            if j * 2 == i: j *= 2
+            if j * 2 == i: 
+                j *= 2
+                
             d[i] = 1 + d[i - j]
-            if d[i] == k: res += nums[i]
+            
+            if d[i] == k: 
+                res += nums[i]
 
         return res
