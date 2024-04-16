be31fefbb73619e98877c57b0a1918ae354c4a23
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-16 20:55:41 +0100

    Time: 40 ms (31.36%), Space: 16.4 MB (89.21%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        
+        counter = 0
+        
+        for pointer in range(len(s)):
+            
+            if pointer > 0:
+                counter += abs(ord(s[pointer]) - ord(s[pointer-1]))
+            
+        return counter
