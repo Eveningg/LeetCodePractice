38b0ac29893df88074ff6fac9dfd9442e235ed3a
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-17 17:46:19 +0100

    Time: 10 ms (82.75%), Space: 11.6 MB (89.00%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,4 +1,9 @@
-class Solution:
-    def toLowerCase(self, s: str) -> str:
-       
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
         return s.lower()
+        
