5e2ab0d6b24c80226b3e197a6a691e26b829dd09
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-21 03:23:10 +0100

    Time: 94 ms (65.63%), Space: 17 MB (20.37%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        
+        counter = 0
+        
+        storePrev = []
+        matrixRowLength = len(mat) - 1
+        
+        for pointer in range(len(mat)):
+            
+            if matrixRowLength - pointer == pointer:
+                counter += mat[pointer][pointer]
+                
+            else:
+                counter += mat[pointer][pointer]
+                counter += mat[pointer][matrixRowLength - pointer]
+            
+        return counter
