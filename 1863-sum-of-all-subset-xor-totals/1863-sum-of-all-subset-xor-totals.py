55690a08caffa341ef3bf0f68e37fa8aa66da649
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-26 08:37:07 +0100

    Time: 39 ms (79.71%), Space: 16.5 MB (84.88%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,13 +1,10 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         
-        ans = 0
-        for mask in range(1 << len(nums)): 
-            val = 0
-            for i in range(len(nums)): 
-                if mask & 1 << i: val ^= nums[i]
-            ans += val
-        return ans 
+        res =0
+        for x in nums:
+            res |= x
+        return res*(2**(len(nums)-1))
                 
         
         
