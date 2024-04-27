a3c4342006e97a9db53b0db12b43b8deb0ecf446
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-27 04:12:21 +0100

    Time: 27 ms (92.84%), Space: 16.6 MB (9.24%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,21 +1,10 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         
-        index = 0
-        newStr = ""
-        
-        for pointer in range(len(word)):
-            
-            if ch == word[pointer]:
-                newStr += word[pointer]
-                firstSlice = word[0:pointer]
-                newStr += firstSlice[::-1]
-                newStr += word[pointer+1:len(word)]
-                break
-                
-            if pointer == len(word)-1:
-                return word
-
-        
-        return newStr
+        prefix = ''
+        for i, c in enumerate(word):
+            if c == ch:
+                return (prefix + c)[::-1] + word[i+1:]
+            prefix += c
+        return prefix
                 
