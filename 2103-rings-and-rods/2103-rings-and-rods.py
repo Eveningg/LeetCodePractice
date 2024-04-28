dbea4934d830eb008ab0aaa64cdb51cfce601f7a
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-28 08:16:01 +0100

    Time: 36 ms (51.74%), Space: 16.5 MB (35.47%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,11 +1,14 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
         
-        c=0
+        counter=0
         l=[""]*10
+        
         for i in range(1,len(rings),+2):
             l[int(rings[i])]+=rings[i-1]
+            
         for j in l:
             if 'R' in j and 'G' in j and 'B' in j:
-                c+=1
-        return c
+                counter += 1
+                
+        return counter
