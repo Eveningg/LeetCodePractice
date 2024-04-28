fa9c3c75d765782acc9c41f1b9778692ff810c25
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-28 08:16:11 +0100

    Time: 35 ms (58.02%), Space: 16.5 MB (35.47%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -2,7 +2,7 @@ class Solution:
     def countPoints(self, rings: str) -> int:
         
         counter=0
-        l=[""]*10
+        l = [""] * 10
         
         for i in range(1,len(rings),+2):
             l[int(rings[i])]+=rings[i-1]
