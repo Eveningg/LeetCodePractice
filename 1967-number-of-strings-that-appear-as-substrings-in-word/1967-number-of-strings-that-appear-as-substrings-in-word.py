12c18bda0150189d8ba4160b35379bc2e3cd7a1d
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-29 09:13:32 +0100

    Time: 81 ms (6.65%), Space: 17 MB (6.44%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -4,6 +4,7 @@ class Solution:
         substrings = []
         total = 0
 
+        # brute-force solution
         for i in range(len(word)):
 
             for j in range(i+1,len(word)+1):
@@ -12,7 +13,5 @@ class Solution:
         for pattern in patterns:
             if pattern in substrings:
                 total += 1
-
-
 
         return total
