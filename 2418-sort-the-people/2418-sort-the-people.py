7a46e7af3ac1829da848a68a87a55313eb2480ec
Evening <93998243+Eveningg@users.noreply.github.com>
2024-04-30 10:07:52 +0100

    Time: 99 ms (81.82%), Space: 17.3 MB (9.01%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,9 +2,9 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
         hash = dict(zip(heights,names))
-        
         names.clear()
         heights.sort(reverse=True)
+        
         for height in heights:
             names.append(hash[height])
             
