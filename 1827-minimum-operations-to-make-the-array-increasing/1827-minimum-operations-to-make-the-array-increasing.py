2bfe0d313b5966c7146694cf7fb70c44534c52da
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-02 11:12:51 +0100

    Time: 98 ms (82.01%), Space: 17.3 MB (80.81%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -4,10 +4,11 @@ class Solution:
         n = len(nums)
         counter = 0
         
-        for i in range(n):
+        for i in range(1,n):
             
-            if nums[i] <= nums[i-1] and i != 0:
+            if nums[i] <= nums[i-1]:
                 counter += (nums[i-1] - nums[i]) + 1
                 nums[i] = nums[i-1] + 1
             
         return counter
+    
