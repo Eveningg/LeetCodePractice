d7ccfc86dc4cbf0f66208b1a8581890bf780233a
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-02 11:12:08 +0100

    Time: 106 ms (36.91%), Space: 17.5 MB (7.38%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -9,7 +9,5 @@ class Solution:
             if nums[i] <= nums[i-1] and i != 0:
                 counter += (nums[i-1] - nums[i]) + 1
                 nums[i] = nums[i-1] + 1
-                
-        print(nums)
-        
+            
         return counter
