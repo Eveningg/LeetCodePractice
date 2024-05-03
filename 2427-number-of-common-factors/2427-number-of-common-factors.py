b34f75a06cbde8add662da259f18d1e1953740af
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-03 11:35:20 +0100

    Time: 37 ms (44.43%), Space: 16.7 MB (13.79%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,6 +1,25 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
         
+        a_factors = []
+        b_factors = []
+        
+        for i in range(1,a+1):
+            if a % i == 0:
+                a_factors.append(i)
+                
+        for i in range(1,b+1):
+            if b % i == 0:
+                b_factors.append(i)
+                
+        counter = 0
+        for i in range(len(b_factors)):
+            
+            if b_factors[i] in a_factors:
+                counter += 1
+                
+        return counter
+    
         count = 0
         minn = min(a,b) # minn = 6
         if max(a,b) % minn == 0: # // 12 % 6 is 0, so count = 1 now
