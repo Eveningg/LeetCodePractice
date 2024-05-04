f00cd6d99c8236359d6dfed12bcfcb40715cb4fe
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-04 17:53:47 +0100

    Time: 163 ms (50.26%), Space: 17.2 MB (23.64%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
@@ -7,3 +7,8 @@ class Solution:
             squares.append(min(i))
             
         return(squares.count(max(squares)))
+    
+        # Explanation:
+        # The Largest Square That Can Be Made
+        # Will Always Be The Smallest Dimensions (Width or Length) stored with the array rectangles.
+        # Then find the largest square in the array squares, and count the amount of instances of it.
