eba95c296e1d360d49a433de81ebd6898bde6f70
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-07 02:26:25 +0100

    Time: 40 ms (31.15%), Space: 16.5 MB (57.71%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,14 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        
+        counter = 0
+        week = 0
+        
+        for i in range(n):
+            
+            if i % 7 == 0 and i != 0:
+                week += 6
+                
+            counter += (i+1) - week
+            
+        return counter
