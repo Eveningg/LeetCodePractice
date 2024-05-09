e06642b9a2ee5ead0590eee26bd196823fe2ae03
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-09 14:58:29 +0100

    Time: 45 ms (8.52%), Space: 16.5 MB (96.67%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+
+    
+        string = ""
+        if n % 2 == 0:
+            string = "p" * (n-1)
+            string += "z"
+        
+        else:
+            string = "h" * n
+            
+        return string
+        
+    
