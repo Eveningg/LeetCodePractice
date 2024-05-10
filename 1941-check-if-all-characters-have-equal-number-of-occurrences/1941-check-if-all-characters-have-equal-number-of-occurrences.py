8d43a4492d25e0f417538a99de54c9d11cf00191
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-10 13:38:59 +0100

    Time: 50 ms (12.42%), Space: 16.7 MB (36.63%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,20 +1,9 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
         
-        hash_map = {}
-        
-        for char in s:
-            
-            if char not in hash_map:
-                hash_map[str(char)] = 1
-            
-            else:
-                hash_map[str(char)] += 1
-        
-        compare = hash_map.get(str(s[0:1]))
-        for value in hash_map.values():
-            if compare != value:
+        amount = s.count(s[0])
+        for i in s:
+            if s.count(i) != amount:
                 return False
         
         return True
-    
