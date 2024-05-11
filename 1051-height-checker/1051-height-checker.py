cec6cfef367e5a96d836f0b4cb354d993039d13a
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-11 22:56:22 +0100

    Time: 41 ms (44.60%), Space: 16.7 MB (7.38%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,11 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        
+        expected = sorted(heights)
+        counter = 0
+        for pointer in range(len(heights)):
+            if heights[pointer] != expected[pointer]:
+                counter += 1
+            
+        
+        return counter
