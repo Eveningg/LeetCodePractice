8a76aeadd981f354c1cf1cdfea811a3308eabab1
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-11 21:22:43 +0100

    Time: 43 ms (46.64%), Space: 16.6 MB (97.98%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -2,14 +2,12 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         
         hash_map = {}
-        
         for i in arr:
             if i not in hash_map:
                 hash_map[i] = 1
                 
             else:
                 hash_map[i] += 1
-        
         
         hash_set = []
         for value in hash_map.values():
