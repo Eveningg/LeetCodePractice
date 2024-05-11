b431f06f2c2e6a8379a756d6feaecdc9833130c0
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-11 23:19:28 +0100

    Time: 28 ms (96.43%), Space: 16.7 MB (17.44%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,7 +1,8 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         
-
+        return len(list(set(nums) - {0}))
+    
         counter = 0
         # Checking Array Has Numbers Greater Than 0
         while max(nums) != 0:
