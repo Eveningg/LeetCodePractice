a3cc6a93e9519f21b73320136bc5975b5ebfcf13
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-11 23:21:33 +0100

    Time: 31 ms (91.95%), Space: 16.8 MB (17.44%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -3,22 +3,7 @@ class Solution:
         
         unique = list(set(nums) - {0})
         return len(unique)
-    
-        counter = 0
-        while max(nums) != 0:
-            counter += 1
-            smallestNonZero = min(i for i in nums if i > 0)
-            for pointer in range(len(nums)):
-                if nums[pointer] != 0:
-                    nums[pointer] -= smallestNonZero
-                    if nums[pointer] < 0:
-                        nums[pointer] = 0
-                
-                
-             
-        
-        return counter
-                
+
                 
 
             
