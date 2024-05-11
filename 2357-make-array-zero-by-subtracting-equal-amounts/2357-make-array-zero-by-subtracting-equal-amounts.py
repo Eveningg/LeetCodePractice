bed895b77511ba248981913e573d75e843a9f471
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-11 23:16:43 +0100

    Time: 47 ms (11.07%), Space: 16.5 MB (89.41%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        
+
+        counter = 0
+        # Checking Array Has Numbers Greater Than 0
+        while max(nums) != 0:
+            counter += 1
+            smallestNonZero = min(i for i in nums if i > 0)
+            for pointer in range(len(nums)):
+                if nums[pointer] != 0:
+                    nums[pointer] -= smallestNonZero
+                    if nums[pointer] < 0:
+                        nums[pointer] = 0
+                
+                
+             
+        
+        return counter
+                
+                
+
+            
+ 
