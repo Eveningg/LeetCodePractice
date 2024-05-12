3ae106b33714aced2883a81a49e02d3579c2f6d5
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-12 01:30:14 +0100

    Time: 44 ms (55.36%), Space: 16.9 MB (7.32%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -5,10 +5,7 @@ class Solution:
         
         for i in range(len(operations)):
 
-            if operations[i] != "C" and operations[i] != "D" and operations[i] != "+":
-                score.append(int(operations[i]))
-            
-            elif operations[i] == "C":
+            if operations[i] == "C":
                 score = score[:len(score)-1]
                 
             elif operations[i] == "D":
@@ -16,12 +13,11 @@ class Solution:
                 score.append(newScore)
                 
             elif operations[i] == "+":
-                a = int(score[len(score)-1])
-                b = int(score[len(score)-2])
-                newScore = a + b
+                newScore = int(score[len(score)-1]) + int(score[len(score)-2])
                 score.append(newScore)
             
-            print(score)
+            else:
+                score.append(int(operations[i]))
         
         return sum(score)
                 
