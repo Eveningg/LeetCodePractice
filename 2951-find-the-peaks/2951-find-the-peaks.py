7c7b902e57caf8bb3b4cc7f30e8ef23d3173b0c2
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-14 17:22:55 +0100

    Time: 50 ms (49.18%), Space: 16.7 MB (11.00%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        
+        hash_set = []
+        n = len(mountain) - 1
+ 
+        for i in range(1,n):
+            print(i)
+            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+                hash_set.append(i)
+        
+        return hash_set
