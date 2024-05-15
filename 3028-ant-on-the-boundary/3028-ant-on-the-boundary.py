db3276438af9fcb7b8e35fcd92fb2a94c80520f1
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-15 23:22:43 +0100

    Time: 39 ms (81.17%), Space: 16.5 MB (46.61%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.py
@@ -0,0 +1,7 @@
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        
+        return sum(n == 0 for n in accumulate(nums))
+                
+            
+                
