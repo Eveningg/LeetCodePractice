c1a5b4270a8905f28e0051102174db1ed0f33cff
Evening <93998243+Eveningg@users.noreply.github.com>
2024-05-18 03:18:23 +0100

    Time: 46 ms (5.52%), Space: 16.5 MB (84.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -9,6 +9,7 @@ class Solution:
         for i in range(n-1):
             newNumber = hash_set[i] + hash_set[i+1]
             hash_set.append(newNumber)
+            print(hash_set)
                 
         return newNumber
             
